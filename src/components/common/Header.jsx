0900fdd4dd85ca593346113a152b0fe33d0f36b2
quietjun802 <whdydwns802@gmail.com>
2025-11-28 19:32:16 +0900

    국장님께 (#8)
    * 메인 홈페이지 구조 조금 만졌어요
    
    * frontend almost end
    
    * frontend user 진짜 끝
    
    * 국장님 한번 해볼게
    
    * 1
    
    * forntend 이미지 바꿈, 로그인 회원가입 페이지 색 변경
    
    * 로그인 페이지 비즈니스 회원가입 추가
    
    * 로그인 페이지 비즈니스 회원가입 추가
    
    * 1
    
    * 1
    
    * 현석아 도커 해봐
    
    * 도커 연결 해봅시다 국장님

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -3,86 +3,98 @@ import { Link } from "react-router-dom";
 import "../../styles/components/common/Header.scss";
 
 const Header = ({ user }) => {
-    const [openMenu, setOpenMenu] = useState(false);
-    const isLoggedIn = !!user;
+  const [openMenu, setOpenMenu] = useState(false);
+  const isLoggedIn = !!user;
 
-    return (
-        <header className="header">
-            <div className="header-left">
-                <Link to="/" className="logo">HotelHub</Link>
+  return (
+    <header className="header">
+      {/* LEFT: 로고 + 메뉴 */}
+      <div className="header-left">
+        <Link to="/" className="logo">HotelHub</Link>
 
-                <nav className="nav">
-                    <Link to="/search">호텔검색</Link>
-                    <Link to="/help">고객센터</Link>
-                </nav>
+        <nav className="nav">
+          <Link to="/search">호텔검색</Link>
+          <Link to="/help">고객센터</Link>
+        </nav>
+      </div>
+
+      {/* RIGHT: 로그인/회원가입 or 프로필 */}
+      <div className="header-right">
+
+        {/* ======================
+            🔹 로그아웃 상태
+        ====================== */}
+        {!isLoggedIn && (
+          <div className="auth-buttons">
+            <Link to="/login" className="btn login-btn">로그인</Link>
+            <Link to="/signup" className="btn signup-btn">회원가입</Link>
+          </div>
+        )}
+
+        {/* ======================
+            🔹 로그인 상태
+        ====================== */}
+        {isLoggedIn && (
+          <div className="logged-in-area">
+
+            {/* ❤️ 찜하기 */}
+            <Link to="/wishlist" className="wish-btn">
+              <i className="fa-regular fa-heart"></i>
+              찜하기
+            </Link>
+
+            {/* 👤 프로필 */}
+            <div
+              className="profile-area"
+              onClick={() => setOpenMenu(!openMenu)}
+            >
+              <div className="profile">
+                <img
+                  src={user.profileImg || "/default_profile.png"}
+                  alt="profile"
+                />
+                <span className="nickname">{user.nickname}</span>
+              </div>
+
+              {/* ======================
+                  🔽 드롭다운 메뉴
+              ====================== */}
+              {openMenu && (
+                <div className="profile-menu">
+                  {/* 상단 유저 정보 */}
+                  <div className="user-info">
+                    <img
+                      src={user.profileImg || "/default_profile.png"}
+                      alt="avatar"
+                      className="avatar"
+                    />
+                    <div className="user-details">
+                      <span className="username">{user.nickname}</span>
+                      <span className="status">Online</span>
+                    </div>
+                  </div>
+
+                  {/* 메뉴 항목 */}
+                  <div className="menu-items">
+                    <Link className="item" to="/mypage/account">계정</Link>
+                    <Link className="item" to="/mypage/payment">결제내역</Link>
+                    <Link className="item" to="/mypage/settings">설정</Link>
+
+                    <div className="divider"></div>
+
+                    <button className="item logout" onClick={() => console.log("logout")}>
+                      로그아웃
+                    </button>
+                  </div>
+                </div>
+              )}
             </div>
 
-            <div className="header-right">
-                {/* 🔹 로그아웃 상태 */}
-                {!isLoggedIn && (
-                    <>
-                        <Link to="/login" className="btn login-btn">로그인</Link>
-                        <Link to="/signup" className="btn signup-btn">회원가입</Link>
-                    </>
-                )}
-
-                {/* 🔹 로그인 상태 */}
-                {isLoggedIn && (
-                    <>
-                        {/* ❤️ 찜하기 */}
-                        <Link to="/wishlist" className="wish-btn">
-                            <i className="fa-regular fa-heart"></i>
-                            찜하기
-                        </Link>
-
-                        {/* 👤 프로필 */}
-                        <div className="profile-area">
-                            <div
-                                className="profile"
-                                onClick={() => setOpenMenu(!openMenu)}
-                            >
-                                <img
-                                    src={user.profileImg || "/default_profile.png"}
-                                    alt="profile"
-                                />
-                                <span>{user.nickname}</span>
-                            </div>
-
-
-
-                            {/* 🔽 드롭다운 메뉴 */}
-                            {openMenu && (
-                                <div className="profile-menu">
-                                    <div className="user-menu">
-                                        <div className="user-info">
-                                            <div className="avatar">T</div>
-                                            <div className="user-details">
-                                                <span className="username">Tomhoon</span>
-                                                <span className="status">Online</span>
-                                            </div>
-                                        </div>
-                                        <div className="menu-items">
-                                            <Link className="item" to="/profile">계정</Link>
-                                            <Link className="item" to="/payments">결제내역</Link>
-                                            <Link className="item" to="/settings">설정</Link>
-
-                                            <div className="divider"></div>
-
-                                            <Link className="item logout" to="/logout">
-                                                로그아웃
-                                            </Link>
-                                        </div>
-
-                                    </div>
-
-                                </div>
-                            )}
-                        </div>
-                    </>
-                )}
-            </div>
-        </header>
-    );
+          </div>
+        )}
+      </div>
+    </header>
+  );
 };
 
 export default Header;
